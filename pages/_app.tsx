688a809c9023e1a6ec1de5e046671f292c916d29
chiol <jcy6677@gmail.com>
2022-03-10 00:54:54 +0900

    feat: head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,34 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { useState } from "react";
-
+import Script from "next/script";
+import Head from "next/head";
+const gaTraceId = "G-0B04M0J7Z2";
 function MyApp({ Component, pageProps }: AppProps) {
     const [queryClient] = useState(() => new QueryClient());
 
     return (
-        <QueryClientProvider client={queryClient}>
-            <Component {...pageProps} />
-        </QueryClientProvider>
+        <>
+            <Script
+                async
+                id="google tag manager"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gaTraceId}`}
+            ></Script>
+            <Script id="google analytics">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${gaTraceId}');
+                `}
+            </Script>
+            <Head>
+                <title>제 20대 대선 실시간 개표 현황</title>
+            </Head>
+            <QueryClientProvider client={queryClient}>
+                <Component {...pageProps} />
+            </QueryClientProvider>
+        </>
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,101 @@
 import { css } from "@emotion/react";
+import {
+    Card,
+    CardContent,
+    CircularProgress,
+    LinearProgress,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    Typography,
+} from "@mui/material";
 import axios from "axios";
 import type { NextPage } from "next";
 import { useQuery } from "react-query";
 
 const Home: NextPage = () => {
     const { data } = useQuery("", () => axios.get("/api/news"), {
-        refetchInterval: 2000,
+        refetchInterval: 1000,
     });
 
-    if (!data) return <></>;
+    if (!data)
+        return (
+            <div
+                css={{
+                    width: "100vw",
+                    height: "200px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}
+            >
+                <CircularProgress />
+            </div>
+        );
+    const isLee =
+        parseFloat(data.data.DUGYUL01) - parseFloat(data.data.DUGYUL02) > 0;
 
     return (
         <div css={containerStyle}>
-            <p>
-                표차이:
-                {(
-                    parseInt(data.data.DUGSU01.replaceAll(",", "")) -
-                    parseInt(data.data.DUGSU02.replaceAll(",", ""))
-                ).toLocaleString()}
+            <p
+                css={{
+                    fontSize: "20px",
+                    color: isLee ? "blue" : "red",
+                }}
+            >
+                {isLee ? "이재명 우세" : "윤석열 우세"}
             </p>
-            <p>
-                개표율:
-                {data.data.GAEPYOYUL}%
-            </p>
-            <table>
-                <thead>
-                    <tr>
-                        <td></td>
-                        <td>표수</td>
-                        <td>비율</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>이재명</td>
-                        <td>{data.data.DUGSU01}</td>
-                        <td>{data.data.DUGYUL01}</td>
-                    </tr>
-                    <tr>
-                        <td>윤석열</td>
-                        <td>{data.data.DUGSU02}</td>
-                        <td>{data.data.DUGYUL02}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <Card>
+                <CardContent>
+                    <p>
+                        득표율 차이:{" "}
+                        {Math.abs(
+                            parseFloat(data.data.DUGYUL01) -
+                                parseFloat(data.data.DUGYUL02)
+                        ).toLocaleString()}
+                        %
+                    </p>
+                    <p>
+                        표차이:{" "}
+                        {Math.abs(
+                            parseInt(data.data.DUGSU01.replaceAll(",", "")) -
+                                parseInt(data.data.DUGSU02.replaceAll(",", ""))
+                        ).toLocaleString()}
+                    </p>
+                    <p>개표율: {data.data.GAEPYOYUL}%</p>
+                </CardContent>
+            </Card>
+            <br />
+            <Card>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell></TableCell>
+                            <TableCell>표수</TableCell>
+                            <TableCell>비율</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>이재명</TableCell>
+                            <TableCell>{data.data.DUGSU01}</TableCell>
+                            <TableCell>{data.data.DUGYUL01}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell>윤석열</TableCell>
+                            <TableCell>{data.data.DUGSU02}</TableCell>
+                            <TableCell>{data.data.DUGYUL02}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </Card>
         </div>
     );
 };
 const containerStyle = css`
-    font-size: 32px;
+    margin: 20px;
 `;
 
 export default Home;
